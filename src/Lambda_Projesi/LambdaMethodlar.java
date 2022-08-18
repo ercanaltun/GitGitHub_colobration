0078826a9601c587fdcea383592637e1e900aa86
Ercan ALTUN <salsel0147@gmail.com>
2022-08-18 13:35:07 +0300

    colobration2

--- src/Lambda_Projesi/LambdaMethodlar.java
+++ src/Lambda_Projesi/LambdaMethodlar.java
@@ -9,12 +9,10 @@ public class LambdaMethodlar {
          **** GitHub_colobration calismasidir *****
 
             **** Görevli Kişiler  Ve   Görevleri****
-         1_Ercan Altun(Teamlead) ==> Asagida verilen List'deki tekrarsiz cift elemenlarin kupunu aynı satırda yazdiran bir method olusuturun(1-20 satır)
+         1_Ercan Altun(Teamlead) ==> Asagida verilen List'deki cift elemenlarin kupunu aynı satırda yazdiran bir method olusuturun(1-40 satır)
          2_Selçuk Kardeş(Tester) ==> Asagida verilen List'deki tekrarsiz tek elemenlarin karesini toplayan bir method olusturun(21-40 satır)
          3_Misafir Hocam(Tester) ==> Asagida verilen List'deki tekrarsiz 3'e bolunen elemenlari carpan bir method olusturun(31-50 satır)
-
  */
-
         List<Integer> liste = new ArrayList<>();
         liste.add(5);
         liste.add(7);
@@ -29,6 +27,47 @@ public class LambdaMethodlar {
         liste.add(8);
         liste.add(2);
 
+         ciftElemanlarinKupunuYazdir(liste);
+        System.out.println();
+
+
+
 
     }
+    public static void ciftElemanlarinKupunuYazdir(List<Integer> mahmut) {
+        mahmut.stream().filter(t->t%2==0).map(t->t*t*t).forEach(t-> System.out.print(t+" "));
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
