34fa1487901ebc8701dfbb4a8df6b2c8ab17b43a
ercanaltun <110672975+ercanaltun@users.noreply.github.com>
2022-08-18 13:42:44 +0300

    Update LambdaMethodlar.java

--- src/Lambda_Projesi/LambdaMethodlar.java
+++ src/Lambda_Projesi/LambdaMethodlar.java
@@ -9,9 +9,9 @@ public class LambdaMethodlar {
          **** GitHub_colobration calismasidir *****
 
             **** Görevli Kişiler  Ve   Görevleri****
-         1_Ercan Altun(Teamlead) ==> Asagida verilen List'deki cift elemenlarin kupunu aynı satırda yazdiran bir method olusuturun(1-40 satır)
-         2_Selçuk Kardeş(Tester) ==> Asagida verilen List'deki tekrarsiz tek elemenlarin karesini toplayan bir method olusturun(21-40 satır)
-         3_Misafir Hocam(Tester) ==> Asagida verilen List'deki tekrarsiz 3'e bolunen elemenlari carpan bir method olusturun(31-50 satır)
+         1_Ercan Altun(Teamlead) ==> Asagida verilen List'deki cift elemenlarin kupunu aynı satırda yazdiran bir method olusuturun(1-50 satır)
+         2_Selçuk Kardeş(Tester) ==> Asagida verilen List'deki tekrarsiz tek elemenlarin karesini toplayan bir method olusturun(51-60 satır)
+         3_Misafir Hocam(Tester) ==> Asagida verilen List'deki tekrarsiz 3'e bolunen elemenlari carpan bir method olusturun(61-70 satır)
  */
         List<Integer> liste = new ArrayList<>();
         liste.add(5);
@@ -26,13 +26,19 @@ public class LambdaMethodlar {
         liste.add(8);
         liste.add(8);
         liste.add(2);
-
-         ciftElemanlarinKupunuYazdir(liste);
+        
+        //Ercan Altun 
+        ciftElemanlarinKupunuYazdir(liste);
         System.out.println();
+        
+        //Selçuk Kardeş
+        
 
 
-
-
+        //Misafir Hocam
+        
+        
+        
     }
     public static void ciftElemanlarinKupunuYazdir(List<Integer> mahmut) {
         mahmut.stream().filter(t->t%2==0).map(t->t*t*t).forEach(t-> System.out.print(t+" "));
@@ -42,6 +48,7 @@ public class LambdaMethodlar {
 
 
 
+    //Selçuk Kardeş
 
 
 
@@ -51,9 +58,9 @@ public class LambdaMethodlar {
 
 
 
+   //Misafir Hocam
 
-
-
+ 
 
 
 
